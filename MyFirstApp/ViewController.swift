dd8555f116bf757dee95316f618ace6ccd47a135
route02 <route02@newnomacbook-air.local>
2018-10-10 19:48:07 +0900

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by route02 on 2018/10/10.
+//  Copyright © 2018年 route02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
