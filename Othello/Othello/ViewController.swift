8c8cf5047f9a85463e68e934b159458eebcb0e5f
shock <kawamura@route-zero.com>
2019-01-16 20:54:05 +0900

    盤面完成

--- Othello/Othello/ViewController.swift
+++ Othello/Othello/ViewController.swift
@@ -9,13 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var black : UIImage? = UIImage(named: "黒石.png")
     @IBOutlet weak var label1: UILabel!
+    @IBAction func test(_ sender: UIButton) {
+        sender.setImage(black, for: UIControl.State.normal)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        label1.text = "a"
+        label1.text = ""
         label1.layer.borderWidth = 1.0
         
     }
