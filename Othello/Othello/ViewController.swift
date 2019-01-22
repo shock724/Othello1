12bf167ffd25d6a7e8008238a06da978355c52a8
shock <kawamura@route-zero.com>
2019-01-23 07:36:26 +0900

    1/22

--- Othello/Othello/ViewController.swift
+++ Othello/Othello/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     var black : UIImage? = UIImage(named: "黒石.png")
     var white : UIImage? = UIImage(named: "白石.png")
+    var turn : Int = 0
+    @IBOutlet var buttonArray: [UIButton] = []
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var startWhite: UIButton!
@@ -18,13 +20,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var startBlack: UIButton!
     @IBOutlet weak var startBlack2: UIButton!
     @IBAction func test(_ sender: UIButton) {
-        sender.setImage(black, for: UIControl.State.normal)
+        let index = buttonArray.index(of: sender)
+        print(index)
+        
+        if sender.image(for: UIControl.State.normal) == nil{
+            if turn == 0{
+                sender.setImage(black, for: UIControl.State.normal)
+                if index! != 7 {
+                    
+                    var count : Int = 1
+                    while(buttonArray[index! + count].image(for: UIControl.State.normal) == white && index! + count != 7){
+                        count += 1
+                    }
+                    
+                    if buttonArray[index! + count].image(for: UIControl.State.normal) == black{
+                        for i in 1 ... count{
+                            buttonArray[index! + i].setImage(black, for: UIControl.State.normal)
+                        }
+                    }
+                }
+                turn += 1
+            }
+                
+            else if turn == 1{
+                sender.setImage(white, for: UIControl.State.normal)
+                turn = 0
+            }
+            
+        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         label1.text = "0"
         label2.text = "0"
         label1.layer.borderWidth = 1.0
